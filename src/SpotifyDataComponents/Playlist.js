83376c66a3270e48875166e5ed5d02c6795a160a
Sarah Murray <sarahmurraydev@gmail.com>
2021-01-01 20:46:38 -0500

    add playlist URL button to card

--- src/SpotifyDataComponents/Playlist.js
+++ src/SpotifyDataComponents/Playlist.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import AppsIcon from '@material-ui/icons/Apps';
+import LinkIcon from '@material-ui/icons/Link';
 import '../App.css';
 import { openModal } from '../actions/actionCreators';
 
@@ -16,13 +17,23 @@ class Playlist extends React.Component {
                 this.props.playlist.images[0]['url']
             ) ? this.props.playlist.images[0]['url'] : "#";
 
+        let playlistURL = this.props.playlist.external_urls && this.props.playlist.external_urls.spotify ? this.props.playlist.external_urls.spotify : "#"
+
         return <div className="playlist-card">
             <img src={smallImg} alt="playlist profile photo returned from spotify" className="playlist-img"/>
             <div className="playlist-details">
                 <div className="name-wrapper">
                     <p className="playlist-name">{this.props.playlist.name}</p>
                 </div>
-                <AppsIcon onClick={() => showPlaylistModal(this.props.playlist)} style={{ color: "#21af43" }}/>
+                <div className="playlist-icons">
+                    <AppsIcon onClick={() => showPlaylistModal(this.props.playlist)} style={{ color: "#21af43" }}/>
+                    <a 
+                        className="app-link inline-link"
+                        href={playlistURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    ><LinkIcon style={{ color: "#21af43" }} /></a>
+                </div>
             </div>
         </div>
     }
